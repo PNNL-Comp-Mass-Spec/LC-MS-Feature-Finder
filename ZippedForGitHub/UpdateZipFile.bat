519c6a4e9ebbf60432522051bf6b283d8c03b3d3
alchemistmatt <matthew.monroe@pnnl.gov>
2020-06-02 12:08:22 -0700

    Add batch file

---
+++ ZippedForGitHub/UpdateZipFile.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\7-Zip\7z.exe" a LCMSFeatureFinder.zip        ..\LCMSFeatureFinder_VB\bin\LCMSFeatureFinder.exe ..\LCMSFeatureFinder_VB\bin\UMCCreation.dll ..\LCMSFeatureFinder_VB\bin\ExampleSettings.ini ..\LCMSFeatureFinder_VB\Data\StartProgram.bat ..\LCMSFeatureFinder_VB\Data\QC_Shew_16_01-500ng_4b_4Jul16_Falcon_16-01-10_isos_Features.txt ..\LCMSFeatureFinder_VB\Data\QC_Shew_16_01-500ng_4b_4Jul16_Falcon_16-01-10_isos_PeakToFeatureMap.txt ..\LCMSFeatureFinder_VB\Data\QC_Shew_16_01-500ng_4b_4Jul16_Falcon_16-01-10_isos.csv ..\LCMSFeatureFinder_VB\Data\QC_Shew_16_01-500ng_4b_4Jul16_Falcon_16-01-10_isos.ini ..\Readme.txt ..\RevisionHistory.txt
+
+pause
